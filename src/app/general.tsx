e6a38da8b62ade10ae0f1f567f004c96b06dd2b2
Cplanchet <masterblasterplanchet@gmail.com>
2026-07-06 15:37:17 -0400

    Add Player Button

--- src/app/general.tsx
+++ src/app/general.tsx
@@ -1,10 +1,24 @@
+import Button from "@/components/common/button";
 import ScoreCard from "@/components/general/score-card";
+import { PlayerScore } from "@/models/score-card";
 import React from "react";
 import { ScrollView, StyleSheet } from "react-native";
 import { ThemedView } from "../../example/src/components/themed-view";
 
 export default function General() {
-  const [score, setScore] = React.useState(0);
+  const [players, setPlayers] = React.useState<PlayerScore[]>([]);
+
+  const addPlayer = () => {
+    const maxId = players.map((p) => p.id).reduce((a, b) => Math.max(a, b), 0);
+    setPlayers([
+      ...players,
+      { id: maxId + 1, name: `Player ${maxId + 1}`, score: 0 },
+    ]);
+  };
+
+  const updatePlayer = (id: number, player: PlayerScore) => {
+    setPlayers(players.map((p) => (p.id === id ? player : p)));
+  };
 
   return (
     <ScrollView
@@ -12,11 +26,17 @@ export default function General() {
       contentContainerStyle={{ flexGrow: 1 }}
     >
       <ThemedView style={styles.container}>
-        <ScoreCard
-          playerName="Player 1"
-          score={score}
-          onScoreChange={setScore}
-        />
+        {players.map((player) => (
+          <ScoreCard
+            key={player.id}
+            playerName={player.name}
+            score={player.score}
+            onScoreChange={(newScore) =>
+              updatePlayer(player.id, { ...player, score: newScore })
+            }
+          />
+        ))}
+        <Button variant="text" onPress={addPlayer} label="Add Player" />
       </ThemedView>
     </ScrollView>
   );
@@ -26,9 +46,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
-    alignItems: "flex-start",
+    alignItems: "center",
     justifyContent: "center",
     flexWrap: "wrap",
     padding: 24,
+    gap: 16,
   },
 });
